d2779735fc84b76b7fa618fc36b5fbca04c2c9ee
Ashish0321 <ashishlekhak2@gmail.com>
2022-07-18 10:34:42 +0545

    muldiv and main added

--- main.py
+++ main.py
@@ -1,2 +1,16 @@
-This is not updated yet
+#This is the main calculator
 
+import Add
+import subtract
+import mul
+import div
+
+a=100
+b=50
+
+addition = Add.add(a,b)
+substraction = substract.sub(a,b)
+multiplication = mul.mul(a,b)
+division = div.div(a,b)
+
+print (f"Addition = {addition} Substraction = {substraction} Multiply = {multiplication} Divide = {division}  "
